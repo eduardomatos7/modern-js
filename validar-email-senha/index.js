882de4eaac724ddc3781842f31d75ef2a1befcd1
eduardomatos7 <eduardo.matoss@ufpe.br>
2024-10-30 10:31:28 -0300

    refactoring logic

--- validar-email-senha/index.js
+++ validar-email-senha/index.js
@@ -1,3 +1,16 @@
+function validateName(name){
+    if (name === ''){
+        const err = new Error("O nome não pode ser vazio")
+        err.input = 'name'
+        throw err
+}else if(name.length < 3){
+        const err = new Error("Digite um nome com pelo menos 3 caracteres")
+        err.input = 'name'
+        throw err
+
+}
+}
+
 function validateEmail(email) {
     if (!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)) {
         const err = new Error('Email inválido.')
@@ -7,13 +20,7 @@ function validateEmail(email) {
 }
 
 function validatePassword(password) {
-    if (
-        password.length < 8 || 
-        !password.match(/[a-z]/) || 
-        !password.match(/[A-Z]/) || 
-        !password.match(/[0-9]/) ||
-        !password.match(/[^a-zA-Z0-9\s]/)
-    ) {
+    if (!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)) {
         const err = new Error('Senha inválida.')
         err.input = 'password'
         throw err
@@ -39,6 +46,7 @@ form.addEventListener('submit', (ev) => {
     ev.preventDefault()
     resetFormStyles()
     try {
+        validateName(userInputs.name.value)
         userInputs.name.classList.add('success')
         validateEmail(userInputs.email.value)
         userInputs.email.classList.add('success')
@@ -47,5 +55,6 @@ form.addEventListener('submit', (ev) => {
     } catch (err) {
         userInputs[err.input].classList.add('error')
         document.querySelector(`#${err.input}-error`).textContent = err.message
+        document.querySelector("#password").value = ""
     }
 })
