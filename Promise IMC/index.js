ba9228ae8f38d86062869db2d350607041a7e0db
eduardomatos7 <eduardo.matoss@ufpe.br>
2024-10-31 23:56:27 -0300

    fixing with async/wait

--- Promise IMC/index.js
+++ Promise IMC/index.js
@@ -1,31 +1,35 @@
-function calcularIMC(peso, altura){
-    return new Promise((resolve, reject)=>{
-        if ((typeof(peso) || typeof(altura)) !== 'number'){
-            reject("Erro! Informe um número inteiro")
-        }else{
-            resolve(peso / (altura * altura))
-        }
-    })
+async function calcularIMC(peso, altura){
+    if (typeof peso !== 'number' || typeof altura !== 'number'){
+        return Promise.reject("Erro! Informe um número inteiro")
+    }
+    return peso / (altura * altura)
+        
 }
-function init(peso, altura){
-    calcularIMC(peso, altura).then((imcValue)=>{
-        console.log(`O resultado do IMC foi de ${imcValue.toFixed(2)}`)
-        if (imcValue < 18.5){
+async function init(peso, altura){
+    try{
+        const resultIMC = await calcularIMC(peso, altura)
+        console.log(`O resultado do IMC foi de ${resultIMC.toFixed(2)}`)
+        if (resultIMC < 18.5){
         console.log('Situação: Magreza')
 
-    }else if (imcValue >= 18.5 && imcValue <= 24.9){
-        console.log("Situação: Normal")
+        }else if (resultIMC >= 18.5 && resultIMC <= 24.9){
+            console.log("Situação: Normal")
 
-    }else if(imcValue >= 25 && imcValue <= 29.9){
-        console.log("Situação: Sobrepeso")
-    }else if(imcValue >= 30 && imcValue <= 39.9){
-        console.log("Situação: Obesidade")
-    }else{
-        console.log("Situação: Obesidade grave")
+        }else if(resultIMC >= 25 && resultIMC <= 29.9){
+            console.log("Situação: Sobrepeso")
+        }else if(resultIMC >= 30 && resultIMC <= 39.9){
+            console.log("Situação: Obesidade")
+        }else{
+            console.log("Situação: Obesidade grave")
+        }
+        }catch(err){
+            console.log(err)
+        }
+        
     }
-    }).catch((error)=>{
-        console.log(error)
-    })
-    console.log("Resolvendo IMC")
-}
-init(77, 1.78)
+    console.log("Resolvendo IMC")    
+        
+init(77, 1.77)
+init(83, 1.80)
+init(50, 1.85)
+
